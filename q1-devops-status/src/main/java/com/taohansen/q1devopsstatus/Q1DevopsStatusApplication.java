5fae4d279483e0a0fc14d722c8ab1558cb58dc5f
taohansens <taohansens@gmail.com>
2024-09-26 22:15:33 -0300

    create service devops-status

---
+++ q1-devops-status/src/main/java/com/taohansen/q1devopsstatus/Q1DevopsStatusApplication.java
@@ -0,0 +1,13 @@
+package com.taohansen.q1devopsstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q1DevopsStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Q1DevopsStatusApplication.class, args);
+    }
+
+}
---
+++ q1-devops-status/src/test/java/com/taohansen/q1devopsstatus/Q1DevopsStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taohansen.q1devopsstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q1DevopsStatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
