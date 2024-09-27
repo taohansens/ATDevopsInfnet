6ac0b41d6b18e728cb4bbb0347f569814b10b375
taohansens <taohansens@gmail.com>
2024-09-26 23:05:44 -0300

    init project q2

---
+++ q2-devops-cars/src/main/java/com/taohansen/q2devopscars/Q2DevopsCarsApplication.java
@@ -0,0 +1,13 @@
+package com.taohansen.q2devopscars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q2DevopsCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Q2DevopsCarsApplication.class, args);
+    }
+
+}
---
+++ q2-devops-cars/src/test/java/com/taohansen/q2devopscars/Q2DevopsCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taohansen.q2devopscars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q2DevopsCarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
